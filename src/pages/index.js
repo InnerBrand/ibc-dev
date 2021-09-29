93bed6179341e075630a3cdb078628499c58f133
InnerBrand <joshua@innerbrandconcierge.com>
2021-09-29 13:33:59 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+        <main>
+          <title>Home Page</title>
+          <h1>Welcome to IBC Dev!</h1>
+          <p>This is being made with Gatsby.js</p>
+        </main>
+      )
+    }
+
+export default IndexPage
